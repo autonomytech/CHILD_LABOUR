56e75a026d7c59e4938fc7330c797c639519e033
Amardeep <amardeep129@gmail.com>
2015-06-04 01:07:27 +0530

    language translation of dashboards done...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
-
+  before_filter :set_locale
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = :mr
   end
 end
